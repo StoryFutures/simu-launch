e060a6c2e2fc2a4bd48d10835bce69b446b29c3d
Andy Woods <andytwoods@users.noreply.github.com>
2022-06-27 07:33:45 +0100

    activator without wifi

--- activator.py
+++ activator.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import requests
 import os
 import time
@@ -8,7 +10,6 @@ while True:
     print(f"{time_only} { ip_address}")
     if ip_address:
         ip_address = ip_address.split(" ")[8]
-        print(ip_address)
 
         os.system("adb tcpip 5555")
 
@@ -18,8 +19,12 @@ while True:
         print('restarting in tcpip mode... waiting 3 seconds')
         time.sleep(3)
         print('letting storystarter know about Quest...')
-        outcome = os.popen("""adb shell "printf 'GET /connect HTTP/1.1\r\n http://192.168.0.155 \r\n\r\n' | nc 192.168.0.155 8000""").read()
-        print(outcome,222)
+        commands = ["adb", "shell",
+                    "printf 'GET /connect HTTP/1.1\r\n http://192.168.0.155 \r\n\r\n'",
+                    "|",
+                    "nc",
+                    "192.168.0.155 8000"]
+        outcome = subprocess.run(commands, stdout=subprocess.PIPE).stdout.decode('ascii')
 
         print("Finished")
     time.sleep(5)
